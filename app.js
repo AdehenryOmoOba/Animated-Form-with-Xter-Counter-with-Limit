b4465db9b4e05496fbe2cf9420913e22205906d7
Adefemi <adefemiadelaja@gmail.com>
2021-01-23 11:15:24 +0100

    commit

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
+// Charater countdown from max limit to zero
+
+// const input = document.querySelector("input");
+// const counter = document.querySelector(".field-counter");
+// const maxLenght = input.getAttribute("maxlength");
+// counter.innerText = maxLenght;
+// input.addEventListener("input", (event) => {
+//   const lengthValue = event.target.value.length;
+//   const countDown = maxLenght - lengthValue;
+//   if (countDown < 0) {
+//     return;
+//   } else {
+//     counter.innerText = countDown;
+//   }
+// });
+
+// Charater counter from Zero to max limit
+
 const input = document.querySelector("input");
 const counter = document.querySelector(".field-counter");
-const maxLenght = input.getAttribute("maxlength");
-counter.innerText = maxLenght;
+counter.innerText = 0;
 input.addEventListener("input", (event) => {
   const lengthValue = event.target.value.length;
-  const countDown = maxLenght - lengthValue;
-  if (countDown < 0) {
+  const countCharacter = lengthValue;
+  if (countCharacter < 0) {
     return;
   } else {
-    counter.innerText = countDown;
+    counter.innerText = countCharacter;
   }
 });
